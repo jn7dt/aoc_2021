353824f5932b8e1e885d07504cb1b8f9c36b6293
jnack <jnack@bluestoneanalytics.com>
2021-12-06 21:28:25 -0500

    day 6 solution. finally to count states rather than generate all fish

--- src/day6.py
+++ src/day6.py
@@ -1,8 +1,45 @@
 import logging
 logger = logging.getLogger('AOC2021')
 
+def simulate_fish(fish_state):
+    new_state = dict()
+    for i in range(1,9):
+        new_state[i-1] = fish_state[i]
+
+    new_state[6] += fish_state[0]
+    new_state[8] = fish_state[0]
+
+    return new_state 
+            
+
 def part1(inputs):
-    pass
+    '''
+    full_timer: 6
+    start_timer: 8
+    end_value: 0
+    '''
+    fish_input = [int(j) for i in inputs for j in i.split(',')]
+    fish_state = {
+        i: 0 for i in range(9)
+    }
+    for fish in fish_input:
+        fish_state[fish] += 1
+
+    days = 80
+    for day in range(days):
+        fish_state = simulate_fish(fish_state)
+    return sum([v for k,v in fish_state.items()])
+
 
 def part2(inputs):
-    pass
+    fish_input = [int(j) for i in inputs for j in i.split(',')]
+    fish_state = {
+        i: 0 for i in range(9)
+    }
+    for fish in fish_input:
+        fish_state[fish] += 1
+
+    days = 256
+    for day in range(days):
+        fish_state = simulate_fish(fish_state)
+    return sum([v for k,v in fish_state.items()])
