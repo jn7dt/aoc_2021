dab9e450fa48c5bc3c9e372adb569c0b23843580
Jason <jason.nack@caci.com>
2022-04-17 22:50:49 -0400

    clean solution

--- src/day7.py
+++ src/day7.py
@@ -12,14 +12,4 @@ def part1(inputs):
 def part2(inputs):
     positions = [int(i) for i in inputs[0].split(',')]
     mean_pos = int(mean(positions))
-    jitter = 1
-    logger.debug(f'mean: {mean_pos}')
-    total_fuel = sum([sum([j for j in range(1, abs(i - mean_pos)+1)]) for i in positions])
-    low_pos = mean_pos - jitter
-    high_pos = mean_pos + jitter
-    low_fuel = sum([sum([j for j in range(1, abs(i - low_pos)+1)]) for i in positions])
-    high_fuel = sum([sum([j for j in range(1, abs(i - high_pos)+1)]) for i in positions])
-    logger.debug(f'Mean Fuel: {total_fuel:,}')
-    logger.debug(f'Low Fuel: {low_fuel:,}')
-    logger.debug(f'High Fuel: {high_fuel:,}')
-    # return total_fuel
+    return sum([sum([j for j in range(1, abs(i - mean_pos)+1)]) for i in positions])
